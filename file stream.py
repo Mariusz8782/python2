8919f5e7ac36521ef6ed1e9eafd77cd938c909b0
Mariusz8782 <amariusz82@gmail.com>
2024-03-19 15:11:06 +0100

    file stream

--- file stream.py
+++ file stream.py
@@ -25,3 +25,19 @@ default            192.168.1.1        UGSc       45        0     en0
 with open('ip_addresses.txt','r')as file:
     contents=file.read()
     print(contents)
+
+with open('routing_table.txt','r')as file:
+    for line in file:
+        print(line.strip())
+
+subnet_masks=['255.255.255.0','255.255.0.0', '255.0.0.0']
+with open('subnet_masks.txt','w')as file:
+    for mask in subnet_masks:
+        file.write(mask+'\n')
+
+print("last exc")
+target_network='192.168.1.0/24'
+with open('routing_table.txt','r')as file:
+    for line in file:
+        if target_network in line:
+            print(line.strip())
